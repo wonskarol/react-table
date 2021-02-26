fe280b4c37052afca9fa8098ee995730aaeaf99d
jawadsef <jawad.sefiani@collibra.com>
2021-02-26 14:20:50 +0100

    fix: replaced default import with named import

--- examples/inno-day-table/src/filtering.js
+++ examples/inno-day-table/src/filtering.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import matchSorter from 'match-sorter'
+import {matchSorter} from 'match-sorter'
 
 function fuzzyTextFilterFn(rows, id, filterValue) {
     return matchSorter(rows, filterValue, { keys: [row => row.values[id]] })
